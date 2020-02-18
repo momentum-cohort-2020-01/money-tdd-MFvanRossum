3cef2e1dfb5dc05d03d2b85f39a3075127575a05
MFvanRossum <matissev@hotmail.com>
2020-02-18 12:57:50 -0500

    Defines parameters of Currensy and Money classes

--- money.py
+++ money.py
@@ -15,7 +15,10 @@ class Currency:
         - symbol - optional symbol used to designate currency
         - digits -- number of significant digits used
         """
-        pass
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+        self.digits = digits
 
     def __str__(self):
         """
@@ -43,7 +46,8 @@ class Money:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        pass
+        self.amount = amount
+        self.currency = currency
 
     def __str__(self):
         """
